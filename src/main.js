3b782b7cebf82911155e8520aa2819bcfbffd047
Alvaro Gabriel Jurado Huarachi <alvaro.jurado@ucb.edu.bo>
2024-10-05 16:46:15 -0400

    Navegacion entre planetas

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
-import './assets/main.css'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router'; // Importa el enrutador
 
-import { createApp } from 'vue'
-import App from './App.vue'
+const app = createApp(App);
 
-createApp(App).mount('#app')
+app.use(router); // Inyecta el router en la aplicación
+
+app.mount('#app');
